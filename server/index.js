55ff97c82cbbf5406b1a2b9bc3bee20bd948ad93
Alfredo Chavez <alfredocharom@gmail.com>
2025-10-31 11:51:42 +0100

    Add server index and basic router

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-'Testing Remotes to Miguel'
+'use strict'
+
+const express = require('express');
+const app = express();
+const serverName = '127.0.0.1';
+const port = 3000;
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./router');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://${serverName}:${port} 🎧`);
+})
